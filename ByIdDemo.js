d0d04d9aa1232330ce0f2a7baa82216b3cfe53da
PrxWrath <pratyushtiwari282@gmail.com>
2022-10-25 12:23:04 +0530

    Added getElementsById demo file

---
+++ ByIdDemo.js
@@ -0,0 +1,8 @@
+const heading = document.getElementById('header-title');
+const formTitle = document.getElementById('form-title');
+
+heading.innerText = "Item Lister Pro"
+heading.style.borderBottom = "3px solid black";
+
+formTitle.style.fontWeight = "bold";
+formTitle.style.color = "green";
